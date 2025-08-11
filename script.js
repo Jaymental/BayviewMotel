92de6f32f71fae91483ea39e56c5ef7efec3a2ab
JohnnyTec <djohnnyhoffmann@gmail.com>
2025-08-11 07:22:18 +0200

    Version 1.0
    Ready to Deploy

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 // Bayview Motel JavaScript-Dokument
 
+function toggleTo(topicId) {
+    const allTopics = document.querySelectorAll('.topic');
+
+    allTopics.forEach(topic => {
+        topic.classList.add('hideEffect');
+        setTimeout(function () { topic.classList.add('hidden') }, 500);
+    });
+
+    const activeTopic = document.getElementById(topicId);
+
+    if (activeTopic) {
+        setTimeout(function () { activeTopic.classList.remove('hidden'); }, 500);
+        setTimeout(function () { activeTopic.classList.remove('hideEffect'); }, 600);
+    }
+}
+
+function toggleMenu(status) {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('visible');
+}
